2036c620bb9597c06826d9f314aed2035b7de5df
Hoanglele <165472920+Hoanglele@users.noreply.github.com>
2024-03-30 17:29:31 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao Anh cứ đi nhầm đường
+    Lạc vào tim Em lẻ loi
+    Anh Biết Có Biết Khi thiếu anh một giây là em như thiếu cả thế giới không??`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, Em Gái à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao anh lại yêu em <3',
+    btnReply: 'Gửi cho em đi <3',
+    reply: 'Em Mãi Yêu Anh Tiến <3333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h em dẫn bảo bối của em đi bật nhé <33.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100074768708728&mibextid=PlNXYD' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
